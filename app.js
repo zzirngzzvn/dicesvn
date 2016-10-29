80c3dbb5d42ee2240d61fcd7387bbababa511507
zzirngzzvn <trantankhoa112@gmail.com>
2016-10-29 22:19:17 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
-  redirect_uri: 'https://dicesvn.github.io',
+  redirect_uri: 'https://zzirngzzvn.github.io/dicesvn/',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: '//socket.moneypot.com',
